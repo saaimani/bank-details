549b9d940b324b904838322cce06b759ab0463a8
saaimani <saaimanis1986@gmail.com>
2022-07-08 18:53:09 +0530

    codd bakup

---
+++ src/org/bank/BankInfo.java
@@ -0,0 +1,15 @@
+package org.bank;
+
+public class BankInfo {
+
+	public void saving() {
+		System.out.println("10,000");
+	}
+	public void fixed() {
+		System.out.println("5000");
+		
+	}
+	public void deposit() {
+		System.out.println("3000");
+	}
+}
